b79bcbcbf4f691348d77649cea107cbae33ff458
George Cardillo Gregorio <george.gregorio@programmers.com.br>
2022-04-12 18:17:18 -0300

    Adapter

---
+++ Adapter/GeradorXml.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Adapter
+{
+    public class GeradorXml
+    {
+        public string GeraXml(object o)
+        {
+            XmlSerializer serializar = new XmlSerializer(o.GetType());
+            StringWriter writer = new StringWriter();
+            serializar.Serialize(writer, o);
+            return writer.ToString();
+        }
+    }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Adapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Cliente cliente = new Cliente();
+            cliente.Nome = "joao";
+            cliente.Endereco = "Rua das flores";
+            cliente.DataNascimento = DateTime.Now;
+
+            string xml = new GeradorXml().GeraXml(cliente);
+
+            Console.WriteLine(xml);
+        }
+    }
+}
